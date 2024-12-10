3fc2931a777abcffd0e3c9285bd7b803c92fb3c9
usy <contact.usyless@gmail.com>
2024-12-10 03:44:00 +0000

    more efficient

--- reels.js
+++ reels.js
@@ -4,74 +4,77 @@
     const Settings = await getSettings();
     let holding = false;
 
-    class Reel {
+    class Video {
+        static addProgressBar(reel) {
+            const dur = reel.duration;
+            const updaters = []
+
+            const barBoxContainer = document.createElement('div');
+            barBoxContainer.classList.add('usy-progress-bar-container');
+
+            const bar = document.createElement('div');
+            bar.classList.add('usy-progress-bar');
+            if (!Settings.preferences.show_bar) bar.classList.add('usy-progress-bar-hidden');
+            barBoxContainer.appendChild(bar);
+            updaters.push((time) => bar.style.width = `${(time / dur) * 100}%`);
+
+            if (Settings.preferences.show_time) {
+                const time = document.createElement('div');
+                time.classList.add('usy-time-count');
+                const timeNode = document.createTextNode(Utils.formatTime(0));
+                time.append(timeNode, document.createTextNode(' / '),
+                    document.createTextNode(Utils.formatTime(dur)));
+                barBoxContainer.appendChild(time);
+                updaters.push((time) => timeNode.textContent = Utils.formatTime(time));
+            }
+
+            reel.after(barBoxContainer);
+
+            const updateBar = (time) => {
+                for (const u of updaters) u(time);
+            }
+
+            reel.addEventListener('timeupdate', () => updateBar(reel.currentTime));
+
+            const updateBarFromMouse = (e) => {
+                const newTime = Math.max(0, Math.min(((e.clientX - barBoxContainer.getBoundingClientRect().left) / barBoxContainer.offsetWidth) * dur, dur));
+                reel.currentTime = newTime;
+                updateBar(newTime);
+            }
+
+            let pauseTimeout = null, paused = false;
+            const pauseReel = reel.pause.bind(reel);
+            barBoxContainer.addEventListener('pointerdown', (e) => {
+                e.preventDefault();
+                paused = reel.paused;
+                if (!paused) pauseTimeout = setTimeout(pauseReel, 150);
+                updateBarFromMouse(e);
+                holding = true;
+            });
+            barBoxContainer.addEventListener('pointermove', (e) => {
+                if (holding) {
+                    e.preventDefault();
+                    updateBarFromMouse(e);
+                }
+            });
+            const stopHold = (e) => {
+                if (holding) {
+                    e.preventDefault();
+                    clearTimeout(pauseTimeout);
+                    if (!paused) reel.play();
+                    holding = false;
+                    updateBarFromMouse(e);
+                }
+            }
+            barBoxContainer.addEventListener('pointerup', stopHold);
+            barBoxContainer.addEventListener('pointerleave', stopHold);
+        }
+
         static addProgressBars() {
             for (const reel of document.body.querySelectorAll('video:not([usy-progress-bar])')) {
-                if (reel.readyState === 4) {
-                    reel.setAttribute('usy-progress-bar', '');
-                    const dur = reel.duration;
-                    const updaters = []
-
-                    const barBoxContainer = document.createElement('div');
-                    barBoxContainer.classList.add('usy-progress-bar-container');
-
-                    const bar = document.createElement('div');
-                    bar.classList.add('usy-progress-bar');
-                    if (!Settings.preferences.show_bar) bar.classList.add('usy-progress-bar-hidden');
-                    barBoxContainer.appendChild(bar);
-                    updaters.push((time) => bar.style.width = `${(time / dur) * 100}%`);
-
-                    if (Settings.preferences.show_time) {
-                        const time = document.createElement('div');
-                        time.classList.add('usy-time-count');
-                        const timeNode = document.createTextNode(Utils.formatTime(0));
-                        time.append(timeNode, document.createTextNode(' / '),
-                            document.createTextNode(Utils.formatTime(dur)));
-                        barBoxContainer.appendChild(time);
-                        updaters.push((time) => timeNode.textContent = Utils.formatTime(time));
-                    }
-
-                    reel.after(barBoxContainer);
-
-                    const updateBar = (time) => {
-                        for (const u of updaters) u(time);
-                    }
-
-                    reel.addEventListener('timeupdate', () => updateBar(reel.currentTime));
-
-                    const updateBarFromMouse = (e) => {
-                        const newTime = Math.max(0, Math.min(((e.clientX - barBoxContainer.getBoundingClientRect().left) / barBoxContainer.offsetWidth) * dur, dur));
-                        reel.currentTime = newTime;
-                        updateBar(newTime);
-                    }
-
-                    let pauseTimeout = null, paused = false;
-                    const pauseReel = reel.pause.bind(reel);
-                    barBoxContainer.addEventListener('pointerdown', (e) => {
-                        e.preventDefault();
-                        paused = reel.paused;
-                        if (!paused) pauseTimeout = setTimeout(pauseReel, 150);
-                        updateBarFromMouse(e);
-                        holding = true;
-                    });
-                    barBoxContainer.addEventListener('pointermove', (e) => {
-                        if (holding) {
-                            e.preventDefault();
-                            updateBarFromMouse(e);
-                        }
-                    });
-                    const stopHold = (e) => {
-                        if (holding) {
-                            e.preventDefault();
-                            clearTimeout(pauseTimeout);
-                            if (!paused) reel.play();
-                            holding = false;
-                            updateBarFromMouse(e);
-                        }
-                    }
-                    barBoxContainer.addEventListener('pointerup', stopHold);
-                    barBoxContainer.addEventListener('pointerleave', stopHold);
-                }
+                reel.setAttribute('usy-progress-bar', '');
+                if (reel.readyState === 4) Video.addProgressBar(reel);
+                else reel.addEventListener('loadeddata', () => Video.addProgressBar(reel));
             }
         }
     }
@@ -81,6 +84,16 @@
             time = parseInt(time);
             return `${Math.floor(time / 60).toString().padStart(2, '0')}:${(time % 60).toString().padStart(2, '0')}`;
         }
+    }
+
+    {
+        const observerSettings = {subtree: true, childList: true};
+        const observer = new MutationObserver((_, o) => {
+            o.disconnect();
+            Video.addProgressBars();
+            o.observe(document.body, observerSettings);
+        });
+        observer.observe(document.body, observerSettings);
     }
 
     async function getSettings() { // Setting handling
@@ -100,6 +113,4 @@
         await set.loadSettings();
         return set;
     }
-
-    setInterval(Reel.addProgressBars, 1000);
 })();
