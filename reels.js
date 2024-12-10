8adf827a357d4c9c2ba2692c9ca7df9c8c20ff93
usy <contact.usyless@gmail.com>
2024-12-10 03:47:25 +0000

    dont pad zeros in first part of time

--- reels.js
+++ reels.js
@@ -82,7 +82,7 @@
     class Utils {
         static formatTime(time) {
             time = parseInt(time);
-            return `${Math.floor(time / 60).toString().padStart(2, '0')}:${(time % 60).toString().padStart(2, '0')}`;
+            return `${Math.floor(time / 60).toString()}:${(time % 60).toString().padStart(2, '0')}`;
         }
     }
 
