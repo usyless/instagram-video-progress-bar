a6e9a5823d8a4e4d37a0184ecd4c1c192f72d15c
usy <contact.usyless@gmail.com>
2024-12-10 01:34:55 +0000

    initial commit

---
+++ reels.js
@@ -0,0 +1,59 @@
+'use strict';
+
+(async () => {
+    class Reel {
+        static addProgressBars() {
+            for (const reel of document.body.querySelectorAll('video:not([usy-progress-bar])')) {
+                if (reel.readyState === 4) {
+                    reel.setAttribute('usy-progress-bar', '');
+                    const dur = reel.duration;
+
+                    const barBoxContainer = document.createElement('div');
+                    barBoxContainer.classList.add('usy-progress-bar-container');
+
+                    const bar = document.createElement('div');
+                    bar.classList.add('usy-progress-bar');
+
+                    const time = document.createElement('div');
+                    time.classList.add('usy-time-count');
+                    const timeNode = document.createTextNode(Utils.formatTime(0));
+                    time.append(timeNode, document.createTextNode(' / '),
+                        document.createTextNode(Utils.formatTime(dur)));
+
+                    barBoxContainer.append(bar, time);
+                    reel.after(barBoxContainer);
+                    reel.addEventListener('timeupdate', () => {
+                        const curr = reel.currentTime;
+                        bar.style.width = `${(curr / dur) * 100}%`;
+                        timeNode.textContent = Utils.formatTime(curr);
+                    });
+                }
+            }
+        }
+    }
+
+    class Utils {
+        static formatTime(time) {
+            time = parseInt(time);
+            return `${Math.floor(time / 60).toString().padStart(2, '0')}:${(time % 60).toString().padStart(2, '0')}`;
+        }
+    }
+
+    const observer = {
+        observer: null,
+        start: () => {
+            observer.observer?.disconnect();
+            const observerSettings = {subtree: true, childList: true},
+                callback = (_, o) => {
+                    o.disconnect();
+                    Reel.addProgressBars();
+                    o.observe(document.body, observerSettings);
+                };
+            observer.observer = new MutationObserver(callback);
+            observer.observer.observe(document.body, observerSettings);
+        }
+    }
+
+    // Starts extension
+    observer.start();
+})();
